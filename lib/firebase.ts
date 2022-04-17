d88613cf8daed0ffd3d99edb39de688f6ef62912
masaya <spag9hc9@yahoo.co.jp>
2022-04-17 23:41:32 +0900

    firebase.tsを修正

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -12,13 +12,9 @@ import 'firebase/firestore'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 if(typeof window !== 'undefined' && getApps().length ===0){
     const firebaseConfig = {
-        apiKey: "AIzaSyC2qpIdiovIxj3nS66uSO1XJJUkm0oXLkE",
-        authDomain: "nextjs-firebase-app-6a8b6.firebaseapp.com",
-        projectId: "nextjs-firebase-app-6a8b6",
-        storageBucket: "nextjs-firebase-app-6a8b6.appspot.com",
-        messagingSenderId: "1047854742243",
-        appId: "1:1047854742243:web:038af35ec95fa077a463d6",
-        measurementId: "G-N9G2RJX091"
+        projectId: process.env.FSA_PROJECT_ID,
+        privateKey: process.env.FSA_PRIVATE_KEY,
+        clientEmail: process.env.FSA_CLIENT_EMAIL,
     };
 
     // Initialize Firebase
